f1792061cf9eae33723a88e534386de7521d0d30
Ashique <ashique12009@gmail.com>
2022-05-31 09:54:13 +0600

    app will be disable in mobile screen

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,40 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div v-if="!mobile">
+    <Navigation msg="Welcome to Your Vue.js App"/>
+  </div>
+  <div v-else>
+    <h2>Sorry, this app is not supported in mobile devices.</h2>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navigation from './components/Navigation.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Navigation
+  },
+  data() {
+    return {
+      mobile: false
+    }
+  },
+  created() {
+    this.checkScreen();
+    window.addEventListener('resize', this.checkScreen);
+  },
+  methods: {
+    checkScreen() {
+      const windowWidth = window.innerWidth;
+      if (windowWidth <= 750) {
+        this.mobile = true;
+        return;
+      }
+      
+      this.mobile = false;
+    }
   }
 }
 </script>
