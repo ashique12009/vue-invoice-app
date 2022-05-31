6122cddbd51df24630c2071f3e004e96d6d18a7f
Ashique <ashique12009@gmail.com>
2022-05-31 12:36:23 +0600

    some basic common css added in app component

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <img alt="Vue logo" src="./assets/logo.png">
   <div v-if="!mobile">
     <Navigation msg="Welcome to Your Vue.js App"/>
+    <router-view/>
   </div>
   <div v-else>
     <h2>Sorry, this app is not supported in mobile devices.</h2>
@@ -39,7 +40,103 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap");
+
+// Reset CSS
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Poppins", sans-serif;
+}
+
+// Common CSS
+button,
+.button {
+  cursor: pointer;
+  padding: 16px 24px;
+  border-radius: 30px;
+  border: none;
+  font-size: 12px;
+  margin-right: 8px;
+  color: #fff;
+}
+
+.dark-purple{
+  background-color: #252945;
+}
+.red{
+  background-color: #ec5757;
+}
+.purple{
+  background-color: #7c5dfa;
+}
+.green{
+  background-color: #33d69f;
+}
+.orange{
+  background-color: #ff8f00;
+}
+
+.flex{
+  display: flex;
+}
+.flex-column{
+  flex-direction: column;
+}
+
+.container{
+  width: 100%;
+  padding: 40px 10px;
+  max-width: 850px;
+  margin: 0 auto;
+  @media (min-width: 900px) {
+    padding-top: 72px;
+  }
+}
+.nav-link {
+  text-decoration: none;
+  color: initial;
+}
+
+// Status Button Styling
+.status-button{
+  &::before {
+    content: "";
+    width: 10px;
+    height: 10px;
+    border-radius: 50%;
+    margin-right: 8px;
+  }
+  font-size: 12px;
+  margin-right: 30px;
+  align-items: center;
+  padding: 8px 30px;
+  border-radius: 10px;
+}
+.paid{
+  &::before{
+    background-color: #33d69f;
+  }
+  color: #33d69f;
+  background-color: rgba(51, 214, 160, 0.1);
+}
+.pending{
+  &::before{
+    background-color: #ff8f00;
+  }
+  color: #ff8f00;
+  background-color: rgba(255, 145, 0, 0.1);
+}
+.draft{
+  &::before{
+    background-color: #dfe3fa;
+  }
+  color: #dfe3fa;
+  background-color: rgba(223, 227, 250, 0.1);
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
